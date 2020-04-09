9ab0542a635f7d2f08efcdcdb65622030d76ddb3
scw3812 <dlffpraos@naver.com>
2020-04-09 23:10:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/prographyandroidstudy/ConsultantActivty.kt
@@ -0,0 +1,12 @@
+package com.example.prographyandroidstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultantActivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultant_activty)
+    }
+}
